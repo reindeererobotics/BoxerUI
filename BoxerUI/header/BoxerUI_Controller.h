691e97542d47d96cfdcb93aafd715b851bbfc83b
tiwaojo <tiwaojo@gmail.com>
2021-06-18 00:12:29 -0400

    camera stream process WIP

--- BoxerUI/header/BoxerUI_Controller.h
+++ BoxerUI/header/BoxerUI_Controller.h
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <string>
 
-
 //#include "BoxerUI_Sockets.h"
 
 class BoxerUI_Controller
@@ -21,7 +20,6 @@ private:
 
 	//BoxerUI_Sockets boxerSocket;
 public:
-
 	double getModelBattery();
 	double getModelTemperature();
 	void setModelBattery(double batteryVal);
@@ -30,19 +28,15 @@ public:
 
 	void cameraPayloadRecv();
 	void decomposePayload(Json::Value jsonPayload);
-	void displayIndexWindow(bool* boxer_analytics);
+	void displayIndexWindow(bool *boxer_analytics);
 	void displayFPS();
 	void demoWindows();
 	void updateBSView();
 	void plotView();
 	void navView();
-
-	void streamCameraView(int* camera);
-	void initCameraView(bool* show_camera, float* w, float* h);
-	void destroyCameraView(int* current_cam);
-
+	void cameraView();
+	//void streamCameraView(int *camera);
+	//void initCameraView(bool *show_camera, float *w, float *h);
+	//void destroyCameraView(int *current_cam);
 };
 //BoxerUI_Controller boxerController = BoxerUI_Controller();
-
-
-
--- Boxer_Install.sh
+++ Boxer_Install.sh
@@ -1,16 +1,42 @@
+#!/bin/bash
 # Install minimal prerequisites (Ubuntu 18.04 as reference)
+sudo apt install -y g++
 sudo apt update && sudo apt install -y cmake g++ wget unzip
+
 # Download and unpack sources
 wget -O opencv.zip https://github.com/opencv/opencv/archive/master.zip
 unzip opencv.zip
 # Create build directory
-mkdir -p build && cd build
-# Configure
-cmake  ../opencv-master
-# Build
-cmake --build .
+mkdir -p opencv-build && cd opencv-build
 
-sudo apt-get install libglfw3-dev
+# Configure
+cmake ../opencv-master/ -DBUILD_LIST=calib3d,core,features2d,flann,highgui,imgcodecs,imgproc,ts,videoio -DBUILD_TESTS=OFF -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=OFF -DWITH_OPENEXR=OFF -DWITH_1394=OFF -DWITH_EIGEN=OFF -DWITH_OPENGL=ON -DBUILD_SHARED_LIBS=OFF -DWITH_FFMPEG=OFF .
+# cmake ../opencv-master/ -DBUILD_LIST=calib3d,core,features2d,flann,highgui,imgcodecs,imgproc,ts,videoio .
+
+# print all options
+cmake -L
+# print all options with help message
+cmake -LH
+# print all options including advanced
+cmake -LA
+
+# Build
+make -j4
+# sudo apt-get install libglfw3-dev
+
+
+mkdir build
+git clone https://github.com/glfw/glfw.git
+
+cd boxer_build
+
+# mkdir ../../build
+# Configure the project. Create the CMakeCache files required for building the project
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc-9 -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++-9 -H. -B../../build
+
+# Build the application
+cmake --build BoxerUI/boxer_build --config Debug --target BoxerUI_app -j 4
 
 chmod +x BoxerUI_app
+
 ./BoxerUI_app
