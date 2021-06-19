3fa8b6ceec6fe28cb5c8502c75b8f93ffe62b1c8
root <root@rain.home>
2021-06-19 01:39:02 -0500

    added header file for computation on video frames

---
+++ BoxerUI/src/Networking/frame_computation.h
@@ -0,0 +1,21 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
+#include <string>
+
+int encode_scale = 50;
+std::vector<int> jpeg = {cv::IMWRITE_JPEG_QUALITY, encode_scale};
+std::vector<int> png = {cv::IMWRITE_PNG_COMPRESSION, encode_scale};
+std::vector<int> encodings[] = {jpeg, png};
+std::string extensions[] = {".jpg", ".png"};
+
+
+std::vector<unsigned char> encodeFrame(cv::Mat new_frame, int encode_style) {
+
+    std::vector<unsigned char> buf;
+
+    cv::imencode(extensions[encode_style], new_frame, buf, encodings[encode_style]);
+
+        return buf;
+}
+
+//decode();
